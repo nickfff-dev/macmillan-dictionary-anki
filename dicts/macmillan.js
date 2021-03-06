61a8f975979b5a6cf1e5cd72f48a107323be2cbf
nickfff-dev <59580486+nickfff-dev@users.noreply.github.com>
2021-03-07 01:44:46 +0300

    Update macmillan.js

--- dicts/macmillan.js
+++ dicts/macmillan.js
@@ -1,87 +1,117 @@
-  if (typeof Macmillan == 'undefined') {
-
-    class Macmillan {
-        constructor() {
-            this.word = '';
-            this.selector = '.SENSE-BODY';
-            this.attr = 'innerHTML';
-            this.base = 'https://www.macmillandictionary.com/us/dictionary/american/'
-
-        }
-
-        resourceURL() {
-            return this.base + encodeURIComponent(this.word);
-        }
-
-        findTerm(word) {
-            this.word = word;
-            let url = this.resourceURL();
-            return this.onlineQuery(url);
-        }
-
-        onlineQuery(url) {
-            return new Promise((resolve, reject) => {
-                $.ajax({
-                    url: url,
-                    type: "GET",
-                    timeout: 5000,
-                    error: (xhr, status, error) => {
-                        reject(error);
-                    },
-                    success: (data, status) => {
-                        let result = this.renderContent(data);
-                        if (result) {
-                            resolve(result);
-                        } else {
-                            reject(new Error('Not Found!'));
-                        }
-                    }
-                });
-            });
-        }
-
-        renderContent(data) {
-            let div = document.createElement("div");
-            div.innerHTML = data;
-            let content = div.querySelector(this.selector);
-            if (content) {
-                let css = this.renderCSS();
-                return css + content[this.attr];
-            } else {
-                return null;
-            }
-        }
-
-        renderCSS() {
-            let css = `
-            <style> 
-                .type-example,
-                .copyright,
-                .inflected_forms,
-                .form {
-                    display:none;
-                }
-                .DEFINITION {
-                    border: 1px solid;
-                    border-color: #e5e6e9 #dfe0e4 #d0d1d5;
-                    border-radius: 3px;
-                    padding: 5px;
-                    margin-top: 3px;
-                }
-                .SENSE-CONTENT {
-                    margin-bottom: 5px;
-                    padding-bottom: 2px;
-                }
-                a {
-                    color: #000;
-                    text-decoration: none;
-                }
-            </style>`;
-
-            return css;
-        }
+/* global api */
+class enen_Macmillan {
+    constructor(options) {
+        this.options = options;
+        this.maxexample = 2;
+        this.word = '';
     }
 
-    registerDict('enen-Macmillan', Macmillan);
+    async displayName() {
+        let locale = await api.locale();
+        if (locale.indexOf('CN') != -1) return '柯林斯英英词典';
+        if (locale.indexOf('TW') != -1) return '柯林斯英英词典';
+        return 'Macmillan English Dictionary';
+    }
 
+
+    setOptions(options) {
+        this.options = options;
+        this.maxexample = options.maxexample;
+    }
+
+    async findTerm(word) {
+        this.word = word;
+        //let deflection = api.deinflect(word);
+        let results = await Promise.all([this.findMacmillan(word)]);
+        return [].concat(...results).filter(x => x);
+    }
+
+    async findMacmillan(word) {
+        let notes = [];
+        if (!word) return notes; // return empty notes
+
+        function T(node) {
+            if (!node)
+                return '';
+            else
+                return node.innerText.trim();
+        }
+
+        let base = 'https://www.macmillandictionary.com/us/dictionary/american/';
+        let url = base + encodeURIComponent(word);
+        let doc = '';
+        try {
+            let data = await api.fetch(url);
+            let parser = new DOMParser();
+            doc = parser.parseFromString(data, 'text/html');
+        } catch (err) {
+            return [];
+        }
+
+        let dictionary = doc.querySelector('div.col-xs-12.col-sm-8.col-md-content-with-right.left-content');
+        if (!dictionary) return notes; // return empty notes
+
+        let expression = T(dictionary.querySelector('span.BASE'));
+        let reading = T(dictionary.querySelector('span.PRON'));
+
+        let band = dictionary.querySelector('span.definition-count-value');
+        let bandnum = band ? band.dataset.band : '';
+        let extrainfo = bandnum ? `<span class="band">${'\u25CF'.repeat(Number(bandnum))}</span>` : '';
+
+        let sound = dictionary.querySelector('span.sound');
+        let audios = sound ? [sound.dataset.srcMp3] : [];
+        // make definition segement
+        let definitions = [];
+        let defblocks = dictionary.querySelectorAll('div.SENSE-BODY') || [];
+        for (const defblock of defblocks) {
+            let pos = T(defblock.querySelector('div.SENSE-NUM'));
+            pos = pos ? `<span class="pos">${pos}</span>` : '';
+            let eng_tran = T(defblock.querySelector('span.DEFINITION'));
+            if (!eng_tran) continue;
+            let definition = '';
+            eng_tran = eng_tran.replace(RegExp(expression, 'gi'), '<b>$&</b>');
+            eng_tran = `<span class='eng_tran'>${eng_tran}</span>`;
+            let tran = `<span class='tran'>${eng_tran}</span>`;
+            definition += `${pos}${tran}`;
+
+            // make exmaple segement
+            let examps = defblock.querySelectorAll('p.EXAMPLE') || '';
+            if (examps.length > 0 && this.maxexample > 0) {
+                definition += '<ul class="sents">';
+                for (const [index, examp] of examps.entries()) {
+                    if (index > this.maxexample - 1) break; // to control only 2 example sentence.
+                    let eng_examp = T(examp) ? T(examp).replace(RegExp(expression, 'gi'), '<b>$&</b>') : '';
+                    definition += eng_examp ? `<li class='sent'><span class='eng_sent'>${eng_examp}</span></li>` : '';
+                }
+                definition += '</ul>';
+            }
+            definition && definitions.push(definition);
+        }
+        let css = this.renderCSS();
+        notes.push({
+            css,
+            expression,
+            reading,
+            extrainfo,
+            definitions,
+            audios,
+        });
+        return notes;
+    }
+
+    renderCSS() {
+        let css = `
+            <style>
+                span.band {color:#e52920;}
+                span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
+                span.tran {margin:0; padding:0;}
+                span.eng_tran {margin-right:3px; padding:0;}
+                span.chn_tran {color:#0d47a1;}
+                ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
+                li.sent  {margin:0; padding:0;}
+                span.eng_sent {margin-right:5px;}
+                span.chn_sent {color:#0d47a1;}
+            </style>`;
+        return css;
+    }
 }
